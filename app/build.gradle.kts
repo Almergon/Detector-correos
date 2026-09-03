17eb6eec460428fb65bfe9e2d21e2ed3f2feec38
Almergon <nene00vk@gmail.com>
2026-09-03 22:16:09 +0200

    Actualizar build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,15 +12,13 @@ android {
         applicationId = "com.example.detectorcorreos"
         minSdk = 26
         targetSdk = 35
-
-        versionCode = 5
-        versionName = "0.5"
+        versionCode = 7
+        versionName = "0.7"
     }
 
     signingConfigs {
         create("release") {
             val keystorePath = System.getenv("DETECTOR_KEYSTORE_PATH")
-
             if (!keystorePath.isNullOrBlank()) {
                 storeFile = file(keystorePath)
                 storePassword = System.getenv("DETECTOR_KEYSTORE_PASSWORD")
@@ -35,41 +33,32 @@ android {
             applicationIdSuffix = ""
             versionNameSuffix = "-debug"
         }
-
         release {
             isMinifyEnabled = false
             signingConfig = signingConfigs.getByName("release")
         }
     }
 
-    buildFeatures {
-        compose = true
-    }
+    buildFeatures { compose = true }
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
 
-    kotlinOptions {
-        jvmTarget = "17"
-    }
+    kotlinOptions { jvmTarget = "17" }
 }
 
 dependencies {
     val composeBom = platform("androidx.compose:compose-bom:2024.12.01")
-
     implementation(composeBom)
     androidTestImplementation(composeBom)
-
     implementation("androidx.core:core-ktx:1.15.0")
     implementation("androidx.activity:activity-compose:1.10.0")
-
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.foundation:foundation")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.material:material-icons-extended")
-
     debugImplementation("androidx.compose.ui:ui-tooling")
 }
