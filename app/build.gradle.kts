a9c2ef1f386324e38b96e7b0b5ebc6e69abe6ffd
Almergon <nene00vk@gmail.com>
2026-09-05 03:06:28 +0200

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         applicationId = "com.example.detectorcorreos"
         minSdk = 26
         targetSdk = 35
-        versionCode = 7
+        versionCode = 8
         versionName = "0.7"
     }
 
